4f299572acf6bf6e00596e4b1e68728a9b38734c
Jordan Jamali <jordanjjamali@gmail.com>
2025-01-27 23:40:42 -0500

    [Broken] Initial commit for OAuth work

--- app/src/main/java/com/bmexcs/pickpic/MainActivity.kt
+++ app/src/main/java/com/bmexcs/pickpic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bmexcs.pickpic
 
+import com.google.firebase.FirebaseApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
