b8ee8a7fcca59223b00aa51157f2824ac814d38d
Jordan Jamali <jordanjjamali@gmail.com>
2025-01-28 00:31:44 -0500

    Remove unused import

--- app/src/main/java/com/bmexcs/pickpic/MainActivity.kt
+++ app/src/main/java/com/bmexcs/pickpic/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.bmexcs.pickpic
 
-import com.google.firebase.FirebaseApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
