5520242b45bba3614efd43d0734daea16beac8bf
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2025-02-11 18:14:26 -0500

    Small thing

--- frontend/app/src/main/java/com/bmexcs/pickpic/presentation/screens/auth/AuthViewModel.kt
+++ frontend/app/src/main/java/com/bmexcs/pickpic/presentation/screens/auth/AuthViewModel.kt
@@ -6,7 +6,5 @@ import com.bmexcs.pickpic.data.auth.AuthRepository
 class AuthViewModel(
     private val authRepository: AuthRepository
 ): ViewModel() {
-//    val isAuthenticated
-//        get() =
-
+    // Not sure what to put here yet
 }
