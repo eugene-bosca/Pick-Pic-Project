c1953467aa5177b13afcac901dd0ae4ca53676b0
Jordan Jamali <jordanjjamali@gmail.com>
2025-02-26 15:04:45 -0500

    Initial database schema implementation

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,10 +2,41 @@ from django.db import models
 import uuid
     
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     display_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    phone = models.CharField(max_length=30)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=30, blank=True, null=True) # Allow null/blank
+    profile_picture = models.ImageField(upload_to='profile_pics') 
+    # TODO: Add default image to profile_picture. Where do we want to store this?
+    # TODO: Confirm upload_to location
 
     def __str__(self):
         return self.display_name
+    
+class UserSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dark_mode = models.BooleanField(default=False)
+
+class EventOwner(models.Model):
+    event = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class EventUser(models.Model):
+    event = models.ForeignKey(EventOwner, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Image(models.Model):
+    image_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    url = models.ImageField(upload_to='event_images')
+    score = models.IntegerField(default=0)
+    # TODO: Confirm upload_to location
+
+class EventContent(models.Model):
+    event = models.ForeignKey(EventOwner, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=255)
+    image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+
+class ScoredBy(models.Model):
+    image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -1,9 +1,54 @@
-from rest_framework import serializers, filters
-from .models import User
+from rest_framework import serializers
+from .models import User, UserSettings, EventOwner, EventUser, Image, EventContent, ScoredBy
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-        filter_backends = [filters.SearchFilter]
-        search_fields = ['username', 'email']
+        fields = ['user_id', 'display_name', 'email', 'phone', 'profile_picture']
+
+
+class UserSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSettings
+        fields = ['user', 'dark_mode']
+
+
+class EventOwnerSerializer(serializers.ModelSerializer):
+    owner_id = UserSerializer()
+
+    class Meta:
+        model = EventOwner
+        fields = ['event', 'owner_id']
+
+
+class EventUserSerializer(serializers.ModelSerializer):
+    event = EventOwnerSerializer()
+    user = UserSerializer()
+
+    class Meta:
+        model = EventUser
+        fields = ['event', 'user']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['image_id', 'url', 'score']
+
+
+class EventContentSerializer(serializers.ModelSerializer):
+    event = EventOwnerSerializer()
+    image_id = ImageSerializer()
+
+    class Meta:
+        model = EventContent
+        fields = ['event', 'event_name', 'image_id']
+
+
+class ScoredBySerializer(serializers.ModelSerializer):
+    image_id = ImageSerializer()
+    user = UserSerializer()
+
+    class Meta:
+        model = ScoredBy
+        fields = ['image_id', 'user']
