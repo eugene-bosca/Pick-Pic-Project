0a2f588352dc204606fcbbe4f6e83cdd08490d7e
eugene-bosca <eugene.bosca@gmail.com>
2025-03-02 18:10:34 -0500

    remove random import

--- frontend/app/src/main/java/com/bmexcs/pickpic/presentation/screens/SupportScreenView.kt
+++ frontend/app/src/main/java/com/bmexcs/pickpic/presentation/screens/SupportScreenView.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.bmexcs.pickpic.presentation.shared.NavigationDrawer
 import androidx.navigation.NavHostController
 
 @Composable
