7b1db0bddcf46a8d1fd7c21caba34c081cced8af
eugene-bosca <eugene.bosca@gmail.com>
2025-03-04 03:29:07 -0500

    wip

--- frontend/app/src/main/java/com/bmexcs/pickpic/presentation/screens/CreateEventScreenView.kt
+++ frontend/app/src/main/java/com/bmexcs/pickpic/presentation/screens/CreateEventScreenView.kt
@@ -1,6 +1,7 @@
 package com.bmexcs.pickpic.presentation.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -21,8 +25,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -39,40 +47,67 @@ fun CreateEventScreenView(
     val context = LocalContext.current
 
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Spacer(modifier = Modifier.fillMaxHeight(0.25f))
-            EditableEventNameField()
-            Spacer(modifier = Modifier.height(50.dp))
-        }
-
-        ElevatedButton(
-            onClick = {
-                viewModel.onCreate(
-                    onSuccess = {
-                        Toast.makeText(context, "Event created successfully!", Toast.LENGTH_SHORT).show()
-                        navController.navigate(Route.CreateEvent.route)
-                    },
-                    onError = { errorMessage ->
-                        Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
-                    }
-                )
-            },
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(
+            painterResource(id = R.drawable.pickpic_landscape),
+            contentDescription = "Background Image",
             modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(bottom = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(0.5f)
+                .clip(RoundedCornerShape(bottomStart = 80.dp, bottomEnd = 80.dp)),
+            contentScale = ContentScale.Crop
+        )
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+            modifier = Modifier
+                .padding(top = LocalConfiguration.current.screenHeightDp.dp * 0.20f)
+                .padding(25.dp),
         ) {
-            Icon(
-                painter = painterResource(R.drawable.add_circle_24px),
-                contentDescription = "Create Event Icon",
-                modifier = Modifier.padding(end = 8.dp)
-            )
-            Text("Create")
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                EditableEventNameField()
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                EditableInviteeNameField()
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                ElevatedButton(
+                    onClick = {
+                        viewModel.onCreate(
+                            onSuccess = {
+                                Toast.makeText(
+                                    context,
+                                    "Event created successfully!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                navController.navigate(Route.CreateEvent.route)
+                            },
+                            onError = { errorMessage ->
+                                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+                            }
+                        )
+                    },
+                    modifier = Modifier.align(Alignment.End)
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.add_circle_24px),
+                        contentDescription = "Create Event Icon",
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text("Create")
+                }
+            }
         }
     }
 }
@@ -97,5 +132,29 @@ fun EditableEventNameField(viewModel: CreateEventViewModel = hiltViewModel()) {
         shape = RoundedCornerShape(16.dp),
         modifier = Modifier.fillMaxWidth(),
         singleLine = true
+    )
+}
+
+@Composable
+fun EditableInviteeNameField() {
+    var email by remember { mutableStateOf("") }
+
+    Text(
+        text = "Invite Users",
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(start = 8.dp)
+    )
+
+    Spacer(modifier = Modifier.height(4.dp))
+
+    OutlinedTextField(
+        value = email,
+        onValueChange = { newValue ->
+            email = newValue },
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        placeholder = { Text("Enter email address") }
     )
 }
