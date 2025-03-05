3b42bcb3c97f9469a16575ac485c6f252f9e1a5d
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2025-03-04 20:02:39 -0500

    PNGPNGPNPNGP NGP GPG PGMG

--- frontend/app/src/main/java/com/bmexcs/pickpic/data/utils/HttpContentType.kt
+++ frontend/app/src/main/java/com/bmexcs/pickpic/data/utils/HttpContentType.kt
@@ -1,19 +1,24 @@
 package com.bmexcs.pickpic.data.utils
 
-import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 
 enum class HttpContentType {
     JSON {
         override fun toString() = "application/json"
-        override fun toMediaType() = toString().toMediaType()
     },
 
     OCTET_STREAM {
         override fun toString() = "application/octet-stream"
-        override fun toMediaType() = toString().toMediaType()
+    },
+
+    PNG {
+        override fun toString() = "image/png"
+    },
+
+    JPEG {
+        override fun toString() = "image/jpeg"
     };
 
     abstract override fun toString(): String
-    abstract fun toMediaType(): MediaType
+    fun toMediaType() = toString().toMediaType()
 }
